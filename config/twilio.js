fd27c5167f0ec349b46b472b065371bbf7115f7c
Rafa <rapala61@gmail.com>
2019-02-07 17:29:35 -0500

    chore (cleanup): Cleanup Twilio env variables

--- config/twilio.js
+++ config/twilio.js
@@ -1,14 +1,9 @@
 'use strict';
 
-const useTestCreds = process.env.TWILIO_API_TEST_CREDS === 'true';
-const envPrefix = useTestCreds ? 'TWILIO_API_TEST_' : 'TWILIO_API_';
-
 const config = {
-  useTestCreds,
-  accountSid: process.env[`${envPrefix}ACCOUNT_SID`] || 'AC-account_sid',
-  // overrided by `config/env/override-test` while testing
-  authToken: process.env[`${envPrefix}AUTH_TOKEN`] || 'totallysecret',
-  from: process.env[`${envPrefix}FROM`] || '+15005550006',
+  accountSid: process.env.TWILIO_API_ACCOUNT_SID || 'AC-account_sid',
+  // overridden by `config/env/override-test` while testing
+  authToken: process.env.TWILIO_API_AUTH_TOKEN || 'totallysecret',
 };
 
 module.exports = config;
