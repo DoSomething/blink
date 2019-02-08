319a719a4075f160129a3cdfa91afd0542994b67
Rafa <rapala61@gmail.com>
2019-02-08 12:24:37 -0500

    troubleshooting (memory): fixed dummy mistake in utilHelper when calculating memory usage

--- src/aws/consumers/lib/util.js
+++ src/aws/consumers/lib/util.js
@@ -5,7 +5,7 @@ const logger = require('../../../../config/logger');
 function logMemUsage() {
   const used = process.memoryUsage();
   Object.keys(used).forEach((key) => {
-    logger.debug(`${key} ${Math.round(used[key] / 1024 / (1024 * 100)) / 100} MB`);
+    logger.debug(`${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`);
   });
 }
 
